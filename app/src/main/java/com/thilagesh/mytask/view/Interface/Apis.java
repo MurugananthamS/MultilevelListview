11fc92a1525e8b9a07d06a4f4419e3e846e7225b
MurugananthamS <muruganantham92@gmail.com>
2019-01-03 13:28:47 +0530

    2nd commit

--- app/src/main/java/com/thilagesh/mytask/view/Interface/Apis.java
+++ app/src/main/java/com/thilagesh/mytask/view/Interface/Apis.java
@@ -1,5 +1,5 @@
 package com.thilagesh.mytask.view.Interface;
 
 public interface Apis {
-    String url = "http://stage.nitash.com/mynitash/common/project/details/384 ";
+    String url = "/*http://stage.nitash.com/mynitash/common/project/details/384*/";
 }
